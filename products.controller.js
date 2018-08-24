1f3ab4ab512ef4babce638e30251d62b1499c273
Rockydonthurtem <charrisq@hotmail.com>
2018-08-24 16:56:28 -0500

    node2

--- products.controller.js
+++ products.controller.js
@@ -1,38 +1,37 @@
+//flow of data - index.js -> Controller -> db then back??
+//continued for higher. Look at the CONNECTION_STRING in massive.
+//That is the 'connection', it reaches out and links the files (heroku, pbweb, db, index, and controller)
 module.exports = {
   create: (req, res, nex) => {
-    console.log(req.body);
+    // console.log(req.body);
     const db = req.app.get("db");
     const { name, description, price, imageurl } = req.body;
-
+    //Just functionality. Same functionality as axios call
     db.create_product([name, description, price, imageurl])
       .then(response => res.sendStatus(200)) //res//
       .catch(err => {
         res.status(500).send(err);
       });
   },
+
   getOne: (req, res, next) => {
     const db = req.app.get("db");
     const { id } = req.params;
     //can I do line 17 instead of solutions const{params} = req followed by db.read_product([params.id])
-    db.read_product(id).then(product =>
-      res
-        .status(200)
-        .send(product)
-
-    ).catch(err => {
-      res.status(500).send("error");
-    });
+    db.read_product(id)
+      .then(product => res.status(200).send(product))
+      .catch(err => {
+        res.status(500).send("error");
+      });
   },
+
   getAll: (req, res, next) => {
     const db = req.app.get("db");
-    db.read_products().then(products =>
-      res
-        .status(200)
-        .send(products)
-    ).catch(err => {
-      res.status(500).send("error");
-    })
-      ;
+    db.read_products()
+      .then(products => res.status(200).send(products))
+      .catch(err => {
+        res.status(500).send("error");
+      });
   },
   //We only send back on get???
   //why wouldn't product be sent back on update??? can there be a product in CB funct and status???
@@ -41,21 +40,24 @@ module.exports = {
     const { id } = req.params;
     const { desc } = req.query;
 
-    db.update_product([id, desc]).then(() => {
-      res
-        .sendStatus(200)
-    }).catch(err => {
-      res.status(500).send("error");
-    });;
+    db.update_product([id, desc])
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(err => {
+        res.status(500).send("error");
+      });
   },
   delete: () => {
     const db = req.app.get("db");
     const { params } = req;
-
-    db.delete_product([params.id]).then(res => {
-      res.sendStatus(200);
-    }).catch(err => {
-      res.status(500).send("error");
-    });
+    //this can be const {id} = req.params
+    db.delete_product([params.id])(id)
+      .then(res => {
+        res.sendStatus(200);
+      })
+      .catch(err => {
+        res.status(500).send("error");
+      });
   }
 };
